70adb368c10792d5f812eaf668f9549b16b63b03
Alberto Sendra <alberto@albertosendra.com>
2014-05-03 11:47:29 +0200

    [FIX] Reorder iTerm install scripts

--- iTerm/install.sh
+++ iTerm/install.sh
@@ -4,6 +4,8 @@ cp .bash_profile ~
 cp .bash_git ~
 cp .git-completion.bash ~
 
+open Solarized-Dark.itermcolors  
+
 cp  com.googlecode.iterm2.plist ~/Library/Preferences/
-open Solarized-Dark.itermcolors
+
 
